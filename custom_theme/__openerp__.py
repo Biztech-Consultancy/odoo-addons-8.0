796504dadc453b6608d0835c0fb2a5dd52d276ab
Unnati Thakkar <unnati@pm.biztechconsultancy.com>
2015-02-26 11:56:39 +0530

    Added changes of theme project

---
+++ custom_theme/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Biztech Theme',
+    'description': 'This module provides feature for changing layout of theme.',
+    'summary': 'This module provides feature for changing layout of theme.',
+    'category': 'Theme',
+    'version': '1.0',
+    'website': 'http://www.biztechconsultancy.com',
+    'author': 'Biztech Consultancy',
+    'depends': ['website'],
+    'data': [
+        'static/src/views/theme.xml',
+    ],
+    'images':[
+            'images/main_screenshot.png',
+    ],
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
