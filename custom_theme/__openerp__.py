ff40e4e08dc623b68e45865eceba1aa5f64167c6
Unnati Thakkar <unnati@pm.biztechconsultancy.com>
2015-04-14 12:42:22 +0530

    Logo changes

--- custom_theme/__openerp__.py
+++ custom_theme/__openerp__.py
@@ -12,7 +12,7 @@
         'static/src/views/theme.xml',
     ],
     'images':[
-            'static/description/icon.png',
+            'static/description/splash_screen.png',
     ],
     'application': True,
 }
